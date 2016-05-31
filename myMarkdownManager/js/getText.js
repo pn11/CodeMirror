316c6aaf2a00e6b3af066db2d27f9d4823f6a281
OKA Naoya <oka@stu.kobe-u.ac.jp>
2016-05-31 20:46:25 +0900

    read and write file were implemented. next is list directory.

--- myMarkdownManager/js/getText.js
+++ myMarkdownManager/js/getText.js
@@ -1,13 +1,15 @@
 //cf:http://codemirror.net/doc/manual.html
 
 function getText() {
-    var output = document.getElementById('output');
+    var output = document.getElementById('log');
     
     window.editor_markdown.save();
     
     //    output.textContent = window.editor_markdown.getTextArea().value;
-    output.textContent = window.editor_markdown.getValue();
+    text = window.editor_markdown.getValue();
+    log.textContent = 'Saved.';
     //    output.textContent = window.editor_markdown.getCode();
+    return text;
 }
 
-window.addEventListener( 'load', init, false );
+//window.addEventListener( 'load', init, false );
